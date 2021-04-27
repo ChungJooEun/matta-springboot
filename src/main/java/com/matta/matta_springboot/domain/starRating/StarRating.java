70cd7e400489f0cd7ba7fc0b1b5be50d4580cd00
ChungJooEun <nina2439@naver.com>
2021-04-28 00:50:19 +0900

    별점 domain 추가

---
+++ src/main/java/com/matta/matta_springboot/domain/starRating/StarRating.java
@@ -0,0 +1,42 @@
+package com.matta.matta_springboot.domain.starRating;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class StarRating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long starRatingId;
+
+    @Column(nullable = false)
+    private int moodRating;
+
+    @Column(nullable = false)
+    private int tasteRating;
+
+    @Column(nullable = false)
+    private int priceRating;
+
+    @Builder
+    public StarRating(int moodRating, int tasteRating, int priceRating){
+        this.moodRating = moodRating;
+        this.tasteRating = tasteRating;
+        this.priceRating = priceRating;
+    }
+
+    public StarRating update(int moodRating, int tasteRating, int priceRating){
+        this.moodRating = moodRating;
+        this.tasteRating = tasteRating;
+        this.priceRating = priceRating;
+
+        return this;
+    }
+
+}
