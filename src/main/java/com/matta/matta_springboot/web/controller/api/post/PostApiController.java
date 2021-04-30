0fc5e4cfb633276eeee88fbe675082429d10bea0
Zio0708 <jiho9478@naver.com>
2021-04-30 22:05:59 +0900

    PostApiController - rest api Get 추가

--- src/main/java/com/matta/matta_springboot/web/controller/api/post/PostApiController.java
+++ src/main/java/com/matta/matta_springboot/web/controller/api/post/PostApiController.java
@@ -1,10 +1,13 @@
 package com.matta.matta_springboot.web.controller.api.post;
 
 import com.matta.matta_springboot.service.post.PostService;
+import com.matta.matta_springboot.web.dto.post.PostListResponseDto;
 import com.matta.matta_springboot.web.dto.post.PostSaveRequestDto;
 import com.matta.matta_springboot.web.dto.post.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,9 +15,13 @@ public class PostApiController {
 
     private final PostService postService;
 
+    @GetMapping("/api/v1/post")
+    public List<PostListResponseDto> getAllPost(){
+        return postService.findAllDesc();
+    }
+
     @PostMapping("/api/v1/post")
     public Long save(@RequestBody PostSaveRequestDto request){
-
         return postService.save(request);
     }
 
